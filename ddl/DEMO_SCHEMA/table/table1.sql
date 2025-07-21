dc898ef523cae89babcd55b6813f90218414c23d
alok882412 <alok.prajapati@doubtnut.com>
2025-07-21 11:59:47 +0530

    Trigger actual run after dry-runs

--- ddl/DEMO_SCHEMA/table/table1.sql
+++ ddl/DEMO_SCHEMA/table/table1.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS DEMO_SCHEMA.table1 (
     id INT PRIMARY KEY,
     name STRING,
     employee STRING,
-    created_at TIMESTAMP
+    department STRING
 );
 
 --changeset ravi:table1-insert
 INSERT INTO DEMO_SCHEMA.table1 (id, name, employee, created_at) 
-VALUES (1, 'Sample Data', 'Vikash', CURRENT_TIMESTAMP);
+VALUES (1, 'Sample Data', 'Vikash', "it");
