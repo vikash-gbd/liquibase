4e43c34d6ebc1b681af8ff10f7881abde9e146a8
alok882412 <alok.prajapati@doubtnut.com>
2025-07-21 12:58:57 +0530

    Initial Liquibase Snowflake setuptzozppp347

--- ddl/DEMO_SCHEMA/table/table1.sql
+++ ddl/DEMO_SCHEMA/table/table1.sql
@@ -5,10 +5,9 @@ CREATE TABLE DEMO_SCHEMA.table1 (
     id INT PRIMARY KEY,
     name STRING,
     employee STRING,
-    created_at TIMESTAMP_NTZ,
-    roll_no   int
+    created_at TIMESTAMP
 );
 
 --changeset ravi:table1-insert runOnChange:true
-INSERT INTO DEMO_SCHEMA.table1 (id, name, employee, created_at,roll_no)
-VALUES (1, 'Sample Data - updated', 'Vikash', CURRENT_TIMESTAMP,11);
+INSERT INTO DEMO_SCHEMA.table1 (id, name, employee, created_at)
+VALUES (2, 'Second Row', 'Ravi', CURRENT_TIMESTAMP);
