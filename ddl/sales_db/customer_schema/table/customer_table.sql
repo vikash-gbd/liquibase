344c6d6ec4dd4e3269d508d42819cc6047fa8b91
alok882412 <alok.prajapati@doubtnut.com>
2025-07-21 22:16:01 +0530

    Liquibase setup with external SQLfinal

--- ddl/sales_db/customer_schema/table/customer_table.sql
+++ ddl/sales_db/customer_schema/table/customer_table.sql
@@ -2,3 +2,8 @@ CREATE OR REPLACE TABLE sales_db.customer_schema.customer_table (
   id INT,
   name STRING
 );
+
+INSERT INTO sales_db.customer_schema.customer_table (id, name) VALUES
+  (1, 'Alice'),
+  (2, 'Bob'),
+  (3, 'Charlie');
