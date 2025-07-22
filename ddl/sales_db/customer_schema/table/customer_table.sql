152a1803bcd24ea20793e9298918041cc30e200e
alok882412 <alok.prajapati@doubtnut.com>
2025-07-22 17:03:08 +0530

    rename SQL to force Liquibase detectionssspqr

--- ddl/sales_db/customer_schema/table/customer_table.sql
+++ ddl/sales_db/customer_schema/table/customer_table.sql
@@ -6,3 +6,8 @@ CREATE OR REPLACE TABLE sales_db.customer_schema.customer_table (
   id INT,
   name STRING
 );
+
+
+--changeset auto:3
+
+ALTER TABLE sales_db.customer_schema.customer_table ADD COLUMN created_at TIMESTAMP_NTZ;
