4801890fb7b19fce83433c89f115d8a6877a995c
alok882412 <alok.prajapati@doubtnut.com>
2025-07-22 17:48:20 +0530

    fix: use sql-check instead of table-exists for doctor_table

--- ddl/sales_db/customer_schema/table/add_doctor_table.sql
+++ ddl/sales_db/customer_schema/table/add_doctor_table.sql
@@ -2,7 +2,11 @@
 
 --changeset auto:5
 --preconditions onFail:MARK_RAN
---precondition-table-exists tableName=DOCTOR_TABLE schemaName=CUSTOMER_SCHEMA
+--precondition-sql-check expectedResult=0
+  SELECT COUNT(*)
+    FROM information_schema.tables
+   WHERE table_schema = 'CUSTOMER_SCHEMA'
+     AND table_name   = 'DOCTOR_TABLE'
 
 CREATE OR REPLACE TABLE sales_db.customer_schema.doctor_table (
   id   INT,
