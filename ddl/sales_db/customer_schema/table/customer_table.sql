aa298cf5dc695fcc13258b30ec6c5a487718bf68
alok882412 <alok.prajapati@doubtnut.com>
2025-07-22 14:20:49 +0530

    Fix: update git push permissions with GH_TOKENsses

--- ddl/sales_db/customer_schema/table/customer_table.sql
+++ ddl/sales_db/customer_schema/table/customer_table.sql
@@ -1,8 +1,10 @@
+--changeset auto:1
+
 CREATE OR REPLACE TABLE sales_db.customer_schema.customer_table (
   id INT,
   name STRING
 );
 
-INSERT INTO sales_db.customer_schema.customer_table (id, name)
-VALUES (6, 'Kiran');
+
+
 
